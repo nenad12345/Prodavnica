5eedb88e6c23a362abb5b3095699b42b038e18df
PC <PC@DESKTOP-FSQQEM7>
2019-04-22 19:49:09 +0200

    hgfdsasdfghj

--- Prodavnica/src/model/Bill.java
+++ Prodavnica/src/model/Bill.java
@@ -7,12 +7,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 @Entity
 public class Bill {
 	@Id	
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int IdBill;
+	@OneToOne
 	private User user;
+	@OneToMany
 	private List<Product> listOfProduct = new ArrayList<Product>();
 	private double totalPrice;
 	
